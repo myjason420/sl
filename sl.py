8b37977ba6f00ace4a21ed7a8aafdd17b60de14b
myjason420 <110984740+myjason420@users.noreply.github.com>
2022-08-10 19:26:17 +0530

    Update sl.py

--- sl.py
+++ sl.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as comp
 
-comp.iframe("https://www.streamlit.com",450,450,True)
+comp.iframe("https://docs.streamlit.io/en/latest",450,450,True)
