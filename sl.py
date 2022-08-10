557a0ac146dc846bde6627d3649941e9c1c90ded
myjason420 <110984740+myjason420@users.noreply.github.com>
2022-08-10 19:30:58 +0530

    Update sl.py

--- sl.py
+++ sl.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as comp
 
-comp.iframe("https://docs.streamlit.io/en/latest",450,450,True)
+comp.iframe("https://api.digitallocker.gov.in/public/oauth2/1/authorize?response_type=code&client_id=1400793F&redirect_uri=https://myjason420-sl-sl-xo5biw.streamlitapp.com/&state=codex1234",450,450,True)
